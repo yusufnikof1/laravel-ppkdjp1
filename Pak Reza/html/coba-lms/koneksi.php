75d6debe31d40b89926517c0aa392ecd501553e4
yusufnikof1 <yusufnikof@gmail.com>
2025-04-21 14:56:12 +0700

    new21042025

---
+++ Pak Reza/html/coba-lms/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'manajemen_modul';
+
+$con = mysqli_connect($host, $user, $pass, $db);
---
+++ absensi-api/routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+
+//check token if exists
+Route::middleware('auth:api')->group(function () {
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+});
